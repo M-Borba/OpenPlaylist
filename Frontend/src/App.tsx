a50c9e5a58a19c714ebf4f7e927c79d7a7794480
ddelgadocds <mborbacds@gmail.com>
2023-07-31 10:10:24 -0300

    build set

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
 import youtubeLogo from '/public/youtube_logo.svg'
 import spotifyLogo from '/public/spotify_logo.svg'
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 import './App.css'
