479eb9aca108c0f432e455cb600f34e4d7563cc1
ddelgadocds <mborbacds@gmail.com>
2023-07-30 22:47:23 -0300

    frontend app created

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import youtubeLogo from '/public/youtube_logo.svg'
+import spotifyLogo from '/public/spotify_logo.svg'
+import { Link, useLocation } from 'react-router-dom';
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://developers.google.com/youtube/v3/getting-started" target="_blank">
+          <img src={youtubeLogo} className="logo" alt="Youtube logo" />
+        </a>
+        <a href="https://developer.spotify.com/documentation/web-api" target="_blank">
+          <img src={spotifyLogo} className="logo spotify" alt="Spotify logo" />
+        </a>
+      </div>
+      <h1>Open Playlist</h1>
+      <div className="card">
+        <Link to="youtube-to-spotify">
+        <button >
+          
+          Move from youtube to spotify
+        </button>
+        </Link>
+
+          <Link to="youtube-to-spotify">
+                    <button >
+          Move from spotify to youtube
+        </button>
+          </Link>
+        <p>
+          Move freely your playlist from one plataform to another .
+        </p>
+      </div>
+      <p className="light-text">
+        Created by Martín Borba
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/main.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "*",
+    element: <p>Error 404 Route Not Found</p>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+   <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
